e3e612bc8a6de0dc473189165e997596d90916f5
Alis025 <98990150+Alis025@users.noreply.github.com>
2023-06-01 14:43:29 -0600

    Initial commit

---
+++ app/src/main/java/com/example/apimaps/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.apimaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var  map:GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createFragment()
+    }
+
+    private fun createFragment() {
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(googleMap: GoogleMap){
+        map = googleMap
+        createMarker()
+    }
+    private fun createMarker(){
+        val coordenadas = LatLng(15.048598,-94.6310605)
+        val marker = MarkerOptions().position(coordenadas).title("Mi Casa")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordenadas, 18f),
+            4000,
+            null
+        )
+    }
+}
